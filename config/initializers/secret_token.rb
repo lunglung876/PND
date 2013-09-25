6e199a2b40bfa82920a1364fbaac2564ab602c12
lunglung876 <lunglung876@gmail.com>
2013-09-25 01:26:56 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PND::Application.config.secret_key_base = '4803ee130772fe2c779e4242a45b994fad46294bcfa23b8689d511cb5167cf74f50eb70375340fa669f0ca0261b67332961df8c41936c9b6b5801299bb2e8467'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PND::Application.config.session_store :cookie_store, key: '_PND_session'
